9b4ae7c72db44e85cc48c03a1f9f99f6f6424399
Wilfredo Villegas <wvillegasm@gmail.com>
2025-05-30 13:12:41 -0400

    Add accessible name to the form

--- src/components/AddCategoryForm.jsx
+++ src/components/AddCategoryForm.jsx
@@ -1,6 +1,19 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 
 const MIN_CATEGORY_LENGTH = 2;
+
+/**
+ * AddCategoryForm component allows users to input and submit a new GIF category.
+ *
+ * @component
+ * @param {Object} props - Component props.
+ * @param {function(string): void} props.onAddCategory - Callback function called with the
+ * formatted category name when a valid category is submitted.
+ *
+ * @example
+ * <AddCategoryForm onAddCategory={handleAddCategory} />
+ */
 
 export const AddCategoryForm = ({ onAddCategory }) => {
   const [gifCategory, setGifCategory] = useState("");
@@ -23,7 +36,12 @@ export const AddCategoryForm = ({ onAddCategory }) => {
   };
 
   return (
-    <form className="space-bottom" onSubmit={handleOnSubmit}>
+    <form
+      aria-label="add-category-form"
+      name="add-category-form"
+      className="space-bottom"
+      onSubmit={handleOnSubmit}
+    >
       <input
         type="text"
         placeholder="Search gifs"
@@ -32,4 +50,8 @@ export const AddCategoryForm = ({ onAddCategory }) => {
       />
     </form>
   );
+};
+
+AddCategoryForm.propTypes = {
+  onAddCategory: PropTypes.func.isRequired,
 };
